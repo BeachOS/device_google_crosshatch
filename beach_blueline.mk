c027598d33dda8a138977845d934613a8e97d205
root <Beach OS>
2023-01-11 08:09:13 +0000

    BeachOS

---
+++ beach_blueline.mk
@@ -0,0 +1,21 @@
+# Inherit some common stuff.
+$(call inherit-product, vendor/beach/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/crosshatch/blueline/device-beach.mk)
+$(call inherit-product, device/google/crosshatch/aosp_blueline.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := beach_blueline
+PRODUCT_MODEL := Pixel 3
+PRODUCT_BRAND := google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=blueline \
+    PRIVATE_BUILD_DESC="blueline-user 12 SP1A.210812.016.C2 8618562 release-keys"
+
+BUILD_FINGERPRINT := google/blueline/blueline:12/SP1A.210812.016.C2/8618562:user/release-keys
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+$(call inherit-product, vendor/google/blueline/blueline-vendor.mk)
